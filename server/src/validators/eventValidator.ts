8ea6c22f89cbd679b7dde9a15aa4e22e440f4998
rd-code-23 <rddeveloper23@gmail.com>
2025-12-04 05:40:25 -0800

    For server, add event validation schemas for creating events and querying events

---
+++ server/src/validators/eventValidator.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const createEventSchema = z.object({
+  title: z.string().min(1),
+  startTimeUTC: z.iso.datetime()
+});
+
+export const getEventsQuerySchema = z.object({
+  date: z.iso.date().optional(),
+  timezone: z.string().optional()
+}).refine(
+  (data) => !(data.date && !data.timezone),
+  {
+    message: "timezone is required when date is provided",
+    path: ["timezone"], // points the error at 'tiemzone'
+  }
+);
+
+export type CreateEventInput = z.infer<typeof createEventSchema>;
+
+export type GetEventsQueryInput = z.infer<typeof getEventsQuerySchema>;
+
+
